d7de52111ccf7949cd0b770b19c752e014de4e98
Vumelani Makhoba <vumelanimakhoba@gmail.com>
2021-11-25 11:00:54 +0200

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "Izinto Cleaning",
+    subtitle: "C# MVC, .Net and Entity Framework",
+    description:
+      "Created a website for booking fumigation, cleaning and gardening services, that also doubles as an online shop for gardening, cleaning and fumigation equipment",
+    image: "./Izinto.gif",
+    link: "https://github.com/AyabongaMakhoba/IzintoCleaning",
+  },
+  {
+    title: "CommuBalance",
+    subtitle: "C# MVC, .Net and Entity Framework",
+    description:
+      "Created online donation service allowing the less priviledged to sign up to receive monhtly food packages secured by QR code confirmation for delivery",
+    image: "./CommuBalance.gif",
+    link: "https://github.com/AyabongaMakhoba/CommuBalance",
+  },
+  {
+    title: "Crooked Art.",
+    subtitle: "C# MVC, .Net and Entity Framework",
+    description:
+      "Created an online clothing shop complete with wishlist and delivery services secured with QR code confirmation",
+    image: "./CrookedArt.gif",
+    link: "https://github.com/AyabongaMakhoba/OnlineClothingStore",
+  },
+  
+];
+
+export const skills = [
+  "C#",
+  "Python",
+  "Html",
+  "CSS",
+  ".NET",
+  "Azure",
+  "Azure Blob Storage",
+  "Git",
+];
